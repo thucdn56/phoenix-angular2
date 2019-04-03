5306f9b1c0e6c966b424a932a31aa897c668e823
ThucDn <thucdongoc@gmail.com>
2019-04-03 09:11:40 +0700

    Lesson 11: create routing for Web Application

--- src/app/main/user/user.module.ts
+++ src/app/main/user/user.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
+import { RouterModule, Routes } from '@angular/router';
+
+export const userRoutes: Routes = [
+  { path: '', redirectTo: 'index', pathMatch: 'full' },
+  { path: 'index', component: UserComponent }];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(userRoutes)
   ],
   declarations: [UserComponent]
 })
