07212b358535395217a8ff4bf59b264172e362cf
ThucDn <thucdongoc@gmail.com>
2019-04-05 11:16:41 +0700

    Lesson 12, 13: Add login, main layout to web application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import {Routes, Router} from '@angular/router'
+import { LoginComponent } from './login/login.component';
 
 export const appRoutes : Routes = [
     { path: "", redirectTo: "login", pathMatch: 'full'},
     { path: 'login', loadChildren: './login/login.module#LoginModule'},
+    // {path: 'login', component: LoginComponent},
     { path: 'main', loadChildren: './main/main.module#MainModule'  }
 ];
